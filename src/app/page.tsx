a61fbc4ddf45be322db56129e51fb4e05135a65a
Thiago Magno <111543335+thgmagno@users.noreply.github.com>
2024-03-06 10:03:45 -0300

    update font, layout, background, and others

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Thiago Magno.</h1>
+    <main className="flex h-screen flex-col items-center justify-center">
+      <div className="rounded-xl border-2 bg-white/5 p-5 text-white">
+        <h1>Thiago Magno.</h1>
+        <b>Projeto em construção... 🛠️</b>
+      </div>
     </main>
   )
 }
